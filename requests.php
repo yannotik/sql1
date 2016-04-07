056615c381d6314e8454095a0afc66493c475af1
Yana <yannotik13@gmail.com>
2016-04-07 20:25:54 +0200

    second commit

--- requests.php
+++ requests.php
@@ -1,18 +1,30 @@
 CREATE database users_db;
 
-CREATE table users;
+DROP database users_db;
 
-INSERT INTO users VALUE (NULL, 'Anna', 'Antonyuk', 'anna@mail.com', 26, 1, 0);
+CREATE TABLE
+    `users` (
+        `id` INT(11) NOT NULL AUTO_INCREMENT,
+        `name` CHAR(35),
+        `surname` CHAR(35),
+        `email` CHAR(60) NOT NULL,
+        `password` CHAR(100) NOT NULL,
+        `age` INT(6),
+        `is_active` INT(10),
+        `is_admin` INT(10),
+        PRIMARY KEY(`id`)
+    );
+
+DELETE FROM users where id = 1;
+
+DROP table users;
+
+INSERT INTO users VALUE (NULL, 'Anna', 'Antonyuk', 'anna@mail.com', 111, 26, 1, 0);
 
 DELETE users_db;
 
-DELETE FROM users;
-
-DROP table users;
 
 CREATE table users;
 SELECT * FROM users;
 UPDATE users SET name = 'Ivan' WHERE id = 2;
 DELETE users_db;
-
-
